d44aaff00a8cb38a0d34aa28b63b4fd818bf3b6e
saikrishna <saikrishnagorantla2001@gmail.com>
2022-09-24 10:53:34 +0530

    modify

--- template.py
+++ template.py
@@ -8,7 +8,7 @@ package_name = "deepClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{package_name}/__init__.py"
+    f"src/{package_name}/__init__.py",
     f"src/{package_name}/components/__init__.py",
     f"src/{package_name}/utils/__init__.py",
     f"src/{package_name}/config/__init__.py",
